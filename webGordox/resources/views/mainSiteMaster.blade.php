c5b06e53cf8e3df41a4394bf05d9ec6592f1f926
Gordox <antonsvenson94@hotmail.se>
2018-05-12 17:02:37 +0900

    Added index view and started working on its content

--- webGordox/resources/views/mainSiteMaster.blade.php
+++ webGordox/resources/views/mainSiteMaster.blade.php
@@ -2,40 +2,52 @@
 <html>
   <head>
     <meta charset="utf-8">
+
     <title>Empty for now</title>
     <!--Test website icon-->
     <link rel="icon" href="https://getbootstrap.com/favicon.ico">
+    <!-- My CSS files -->
+    <link rel="stylesheet" href="../../css/GordoxCSS.css">
 
     <!--bootstrap-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
   </head>
 
-  <body>
-    <div class="container">
-
-      <!-- Top menu -->
-      <header>
-        <div>
-            <div class="card text-white">
-              <!-- Top page Image -->
-              <img src="http://demos.cryoutcreations.eu/wordpress/fluida/wp-content/uploads/2013/02/jellyfish-698521-1920x250.jpg"
-                   class="card-img" alt="Responsive image">
-
-              <!-- Login -->
-               <div class="card-img-overlay">
-                 <a class="card-title" style="float: right;" href="#">Login</a>
-               </div>
+  <body class="BodyBackground">
+    <!-- Top menu -->
+    <header>
+      <!-- Image & Login-->
+      <div class="container">
+        <div class="row headerImage">
+            <div class="col">
             </div>
 
-          <!-- Navbar menu -->
-          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <!-- Login -->
+            <div class="col-sm-2">
+             <div class="card-img-overlay loginText">
+               <a class="card-title" href="#">Login</a>
+               @if (Route::has('login'))
+                       @if (Auth::check())
+
+                       @else
+
+                       @endif
+               @endif
+             </div>
+           </div>
+          </div>
+        </div>
+
+      <!--Navbar menu-->
+      <div class="container no-padding">
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
+              <ul class="navbar-nav ml-auto" >
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item dropdown">
+                  <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Works
                   </a>
@@ -54,14 +66,24 @@
               </ul>
             </div>
           </nav>
-
         </div>
-      </header>
+    </header>
 
-    </div>
+    <!-- Content -->
+    <main role="main">
+      <div class="container no-padding">
+        @yield('content')
+      </div>
+    </main>
 
-    <footer class="">
-      <p>hej ajaja</p>
+   <!-- Bottom -->
+    <footer class="text-muted MyFooter">
+      <div class="container no-padding">
+        <p class="float-right">
+          <a href="#">Back to top</a> <!--Make a buttom if it later -->
+        </p>
+        <p>&copy;2018 By Anton Svensson</p>
+      </div>
     </footer>
 
     <!-- JavaScript -->
--- webGordox/routes/web.php
+++ webGordox/routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('mainSiteMaster');
+    return view('index');
 });
