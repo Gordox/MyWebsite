bcdc067447372645093cbcca6a7d5c503504fa1e
Gordox <antonsvensson94@hotmail.se>
2018-07-31 13:33:24 +0000

    Update site

--- webGordox/public/index.php
+++ webGordox/public/index.php
@@ -19,10 +19,11 @@ define('LARAVEL_START', microtime(true));
 | into the script here so that we don't have to worry about manual
 | loading any of our classes later on. It feels great to relax.
 |
+| require __DIR__.'/../webGordox/bootstrap/autoload.php';//try1
+| vendor
 */
 
 require __DIR__.'/../vendor/autoload.php';
-
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
@@ -32,6 +33,8 @@ require __DIR__.'/../vendor/autoload.php';
 | This bootstraps the framework and gets it ready for use, then it
 | will load up this application so that we can run it and send
 | the responses back to the browser and delight our users.
+|
+| $app = require_once __DIR__.'/../webGordox/bootstrap/app.php'; //try1
 |
 */
 
@@ -58,3 +61,4 @@ $response = $kernel->handle(
 $response->send();
 
 $kernel->terminate($request, $response);
+
